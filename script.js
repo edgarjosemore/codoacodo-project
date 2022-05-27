1b7b0f8c19a680df3060a2d71b5fa3f8f2508259
edgarjosemore <edgarjosemoreno17@gmail.com>
2022-05-26 23:11:47 -0300

    Proyecto Codo a Codo

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var getData = function(){
+    var nombres=document.getElementById("nombres").value; 
+    var apellidos=document.getElementById("apellidos").value;
+    var correo=document.getElementById("correo").value;
+    var telefono=document.getElementById("telefono").value;
+    console.log(nombres + " " + apellidos + " " + correo + " " + telefono);
+}
